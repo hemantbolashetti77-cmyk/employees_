d87f2369ee3db219a085dbd3058bcff45339ebe6
hemant17 <hemantbolashetti77@gmail.com>
2026-01-12 16:10:31 +0530

    Update test_emp.py

--- test_emp.py
+++ test_emp.py
@@ -1,22 +1,13 @@
-import unittest
 from emp import calculate_bonus
 
-class TestEmployeeBonus(unittest.TestCase):
+def test_bonus_26():
+    assert calculate_bonus(26) == 5000
 
-    def test_bonus_26_days(self):
-        self.assertEqual(calculate_bonus(26), 5000)
+def test_bonus_20():
+    assert calculate_bonus(20) == 3000
 
-    def test_bonus_20_days(self):
-        self.assertEqual(calculate_bonus(20), 3000)
+def test_bonus_15():
+    assert calculate_bonus(15) == 1500
 
-    def test_bonus_15_days(self):
-        self.assertEqual(calculate_bonus(15), 1500)
-
-    def test_bonus_less_than_15(self):
-        self.assertEqual(calculate_bonus(10), 0)
-
-    def test_bonus_edge_case(self):
-        self.assertEqual(calculate_bonus(0), 0)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_bonus_10():
+    assert calculate_bonus(10) == 0
